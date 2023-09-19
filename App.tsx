952ada79ca39c2f478053fb4f22b0b003369212b
MartinB04 <martin.bibiano4@outlook.com>
2023-09-19 12:54:20 -0600

    BoxObjectModelScreen.tsx agregado

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 import { ContadorScreen } from './src/screens/ContadorScreen';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 
 export const App = () => {
   return (
-    <ContadorScreen/>
+    <BoxObjectModelScreen/>
   );
 };
---
+++ src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const BoxObjectModelScreen = () => {
+  return (
+    <>
+      <Text>BoxObjectModelScreen</Text>
+    </>
+  );
+};
