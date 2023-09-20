46aa23edc7b7f2239631494ca95315aa9ff95890
MartinB04 <martin.bibiano4@outlook.com>
2023-09-19 22:55:02 -0600

    TareaScreen.tsx: requisitos de tarea agregados

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 
 export const TareaScreen = () => {
   return (
-    <div>TareaScreen</div>
+    <View style={styles.container}>
+        <View style={styles.cajaMorada}/>
+        <View style={styles.cajaNaranja}/>
+        <View style={styles.cajaAzul}/>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#37d47e",
+
+    },
+    cajaMorada: {
+        width:100,
+        height:100,
+        borderColor: "white",
+        borderWidth: 10,
+        backgroundColor: "purple",
+    },
+    cajaNaranja: {
+        width:100,
+        height:100,
+        borderColor: "white",
+        borderWidth: 10,
+        backgroundColor: "orange",
+    },
+    cajaAzul: {
+        width:100,
+        height:100,
+        borderColor: "white",
+        borderWidth: 10,
+        backgroundColor: "blue",
+    },
+})
