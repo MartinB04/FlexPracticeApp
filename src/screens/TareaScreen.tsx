7dc4952e6bcfdcd8d06559e6800d75d67abed8e0
MartinB04 <martin.bibiano4@outlook.com>
2023-09-19 23:34:49 -0600

    Ejercicio 5: terminado

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -15,36 +15,36 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#37d47e",
-        justifyContent:"center",
-        flexDirection:'column'
+
+        flexDirection:'row',
+        justifyContent:'space-between'
 
     },
     cajaMorada: {
         width:100,
-        height:100,
+ 
         borderColor: "white",
         borderWidth: 10,
         backgroundColor: "purple",
-        position: 'absolute',
-        right: 0,
-        top: 0,
+     
+
     },
     cajaNaranja: {
         width:100,
-        height:100,
+
         borderColor: "white",
         borderWidth: 10,
         backgroundColor: "orange",
-        alignSelf: 'center'
+    
+ 
     },
     cajaAzul: {
         width:100,
-        height:100,
         borderColor: "white",
         borderWidth: 10,
         backgroundColor: "blue",
-        position: 'absolute',
-        bottom:0,
+  
+
         
     },
 })
