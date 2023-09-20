67bfeee542b02a55009e6228b130ca703d30775b
MartinB04 <martin.bibiano4@outlook.com>
2023-09-19 23:38:26 -0600

    Ejercicio 6: terminado

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -1,50 +1,39 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 
 export const TareaScreen = () => {
   return (
     <View style={styles.container}>
-        <View style={styles.cajaMorada}/>
-        <View style={styles.cajaNaranja}/>
-        <View style={styles.cajaAzul}/>
+      <View style={styles.cajaMorada} />
+      <View style={styles.cajaNaranja} />
+      <View style={styles.cajaAzul} />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#37d47e",
+  container: {
+    flex: 1,
+    backgroundColor: '#37d47e',
 
-        flexDirection:'row',
-        justifyContent:'space-between'
-
-    },
-    cajaMorada: {
-        width:100,
- 
-        borderColor: "white",
-        borderWidth: 10,
-        backgroundColor: "purple",
-     
-
-    },
-    cajaNaranja: {
-        width:100,
-
-        borderColor: "white",
-        borderWidth: 10,
-        backgroundColor: "orange",
-    
- 
-    },
-    cajaAzul: {
-        width:100,
-        borderColor: "white",
-        borderWidth: 10,
-        backgroundColor: "blue",
-  
-
-        
-    },
-})
+    flexDirection: 'column',
+  },
+  cajaMorada: {
+    flex: 2.5,
+    borderColor: 'white',
+    borderWidth: 10,
+    backgroundColor: 'purple',
+  },
+  cajaNaranja: {
+    flex: 2.5,
+    borderColor: 'white',
+    borderWidth: 10,
+    backgroundColor: 'orange',
+  },
+  cajaAzul: {
+    flex: 5,
+    borderColor: 'white',
+    borderWidth: 10,
+    backgroundColor: 'blue',
+  },
+});
